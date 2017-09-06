1947c1e466b381328cec5d8dae988f32db5eb838
Kyeongdon Kim <kyeongdon.kim@lge.com>
2017-09-06 18:50:19 +0900

    selinux: Use kmem_cache for hashtab_node
    During random test as own device to check slub account,
    we found some slack memory from hashtab_node(kmalloc-64).
    By using kzalloc(), middle of test result like below:
    allocated size 240768
    request size 45144
    slack size 195624
    allocation count 3762
    
    So, we want to use kmem_cache_zalloc() and that
    can reduce memory size 52byte(slack size/alloc count) per each struct.
    
    Signed-off-by: Kyeongdon Kim <kyeongdon.kim@lge.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -2025,10 +2025,12 @@ int security_load_policy(void *data, size_t len)
 	if (!ss_initialized) {
 		avtab_cache_init();
 		ebitmap_cache_init();
+		hashtab_cache_init();
 		rc = policydb_read(&policydb, fp);
 		if (rc) {
 			avtab_cache_destroy();
 			ebitmap_cache_destroy();
+			hashtab_cache_destroy();
 			goto out;
 		}
 
@@ -2040,6 +2042,7 @@ int security_load_policy(void *data, size_t len)
 			policydb_destroy(&policydb);
 			avtab_cache_destroy();
 			ebitmap_cache_destroy();
+			hashtab_cache_destroy();
 			goto out;
 		}
 
@@ -2048,6 +2051,7 @@ int security_load_policy(void *data, size_t len)
 			policydb_destroy(&policydb);
 			avtab_cache_destroy();
 			ebitmap_cache_destroy();
+			hashtab_cache_destroy();
 			goto out;
 		}
 
