6bc10d7b719010465755bbe0601a014bfad8c1bd
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-01-24 10:46:23 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: If04d5d50971a18652a0f3dde22dca8725d20a632
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12155,8 +12155,6 @@ static void walt_check_for_rotation(struct rq *src_rq)
 	if (!walt_rotation_enabled)
 		return;
 
-	if (got_boost_kick())
-		return;
 
 	if (is_max_capacity_cpu(src_cpu))
 		return;
