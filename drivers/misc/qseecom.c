3ebbe28a39ffed201976ccaa4d6872ac5ac00a15
Yousef Algadri <yusufgadrie@gmail.com>
2019-06-05 07:44:33 +0700

    firmware/tz_log: Revert Xiaomi changes
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -440,8 +440,6 @@ static int __qseecom_scm_call2_locked(uint32_t smc_id, struct scm_desc *desc)
 	return ret;
 }
 
-extern void read_qseelog_wakeup(void);
-
 static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 			const void *req_buf, void *resp_buf)
 {
@@ -1100,7 +1098,6 @@ static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 	pr_debug("scm_resp->result = 0x%x, scm_resp->resp_type = 0x%x, scm_resp->data = 0x%x\n",
 		scm_resp->result, scm_resp->resp_type, scm_resp->data);
 
-	read_qseelog_wakeup();
 	return ret;
 }
 
