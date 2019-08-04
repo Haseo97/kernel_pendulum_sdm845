3f24964d0108a151be3b371e439ef5e4f8c968e4
Yousef Algadri <yusufgadrie@gmail.com>
2019-08-05 05:03:32 +0700

    cpufreq: schedutil: Initialize up_rate/down_rate to 0
    * https://android-review.googlesource.com/c/kernel/common/+/444322
    
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -736,7 +736,6 @@ static int sugov_init(struct cpufreq_policy *policy)
 {
 	struct sugov_policy *sg_policy;
 	struct sugov_tunables *tunables;
-	unsigned int lat;
 	int ret = 0;
 
 	/* State should be equivalent to EXIT */
@@ -775,13 +774,14 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = 500;
-	tunables->down_rate_limit_us = 1000;
-	lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
-	if (lat) {
-		tunables->up_rate_limit_us *= lat;
-		tunables->down_rate_limit_us *= lat;
-	}
+	/*
+	 * NOTE:
+	 * intializing up_rate/down_rate to 0 explicitly in kernel
+	 * since WALT expects so by default.
+	 */
+
+	tunables->up_rate_limit_us = 0;
+	tunables->down_rate_limit_us = 0;
 
 	tunables->iowait_boost_enable = false;
 	policy->governor_data = sg_policy;
