554e7ce6b6dad8062647a34fcca7640eca00b08b
Yousef Algadri <yusufgadrie@gmail.com>
2019-08-21 12:56:46 +0700

    platform: msm: gsi: Optionally remove debug
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- drivers/platform/msm/gsi/gsi.c
+++ drivers/platform/msm/gsi/gsi.c
@@ -3146,7 +3146,9 @@ static int msm_gsi_probe(struct platform_device *pdev)
 #endif
 	gsi_ctx->dev = dev;
 	init_completion(&gsi_ctx->gen_ee_cmd_compl);
+#ifdef CONFIG_DEBUG_KERNEL
 	gsi_debugfs_init();
+#endif
 
 	return 0;
 }
