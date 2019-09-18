d97a276a9fb5050749ea0da0d328c767df2b476d
Yousef Algadri <yusufgadrie@gmail.com>
2019-09-18 20:26:51 +0700

    Revert "sched/fair: Turn off EAS_USE_NEED_IDLE sched feature"
    This reverts commit 4bebe84397d4d1eb5d5392cbd7f731568dd4331b.
    
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -101,7 +101,7 @@ SCHED_FEAT(FBT_STRICT_ORDER, false)
  * ON: schedtune.prefer_idle is replaced with need_idle
  * OFF: schedtune.prefer_idle is honored as is.
  */
-SCHED_FEAT(EAS_USE_NEED_IDLE, false)
+SCHED_FEAT(EAS_USE_NEED_IDLE, true)
 
 
 /*
