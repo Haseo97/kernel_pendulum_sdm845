f75ecec0be07dd178a09505300ad184d18337cd7
Daniel Micay <danielmicay@gmail.com>
2019-10-15 04:44:04 -0400

    mark functions with address taken via assembly
    This fixes compatibility with CFI in a build with !CONFIG_MODULES.
    
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -17,6 +17,7 @@
  */
 
 #include <linux/types.h>
+#include <asm/alternative.h>
 #include <asm/cachetype.h>
 #include <linux/cpu.h>
 #include <asm/cpu.h>
@@ -260,6 +261,12 @@ void __init arm64_update_smccc_conduit(struct alt_instr *alt,
 	*updptr = cpu_to_le32(insn);
 }
 
+__attribute__((used))
+alternative_cb_t arm64_update_smccc_conduit_indirectly_callable(void)
+{
+	return &arm64_update_smccc_conduit;
+}
+
 void __init arm64_enable_wa2_handling(struct alt_instr *alt,
 				      __le32 *origptr, __le32 *updptr,
 				      int nr_inst)
@@ -272,6 +279,12 @@ void __init arm64_enable_wa2_handling(struct alt_instr *alt,
 	 */
 	if (arm64_get_ssbd_state() == ARM64_SSBD_KERNEL)
 		*updptr = cpu_to_le32(aarch64_insn_gen_nop());
+}
+
+__attribute__((used))
+alternative_cb_t arm64_enable_wa2_handling_indirectly_callable(void)
+{
+	return &arm64_enable_wa2_handling;
 }
 
 void arm64_set_ssbd_mitigation(bool state)
